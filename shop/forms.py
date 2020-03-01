5789bed89e679025249cf9e91b5cf98390ae158d
covee <uniqtop91@gmail.com>
2020-03-01 19:45:05 +0900

    restart

--- shop/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class CForm(forms.Form):
-	name = forms.CharField()
-	
--- shop/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import FormView
-from .forms import CForm
-
-
-class CView(FormView):
-	template_name = 'main.html'
