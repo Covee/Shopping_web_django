4dfe3b5ba03befb5cffff1e0b90a1b5fa033cf08
covee <uniqtop91@gmail.com>
2020-03-05 11:39:18 +0900

    pillow 설치, dish model 정의

--- trustDish/dish/models.py
+++ trustDish/dish/models.py
@@ -2,11 +2,29 @@ from django.db import models
 
 
 class Dish(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=30)
+    price = models.IntegerField()
+    portion = models.IntegerField()
+    thumbnail = models.ImageField()
+    image = models.ImageField()
     description = models.TextField()
+    
 
     def __str__(self):
         return self.name
     
 
-# class 
+class DishReview(models.Model):
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    title = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class DishQA(models.Model):
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    title = models.TextField()
+
+    def __str__(self):
+        return self.title 
