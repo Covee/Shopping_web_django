86c51609577f2661d23fb67b2614de4a9424c655
covee <uniqtop91@gmail.com>
2020-03-06 15:31:52 +0900

    admin, url 기본 설정

--- trustDish/dish/admin.py
+++ trustDish/dish/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Dish, DishReview, DishQA
 
-# Register your models here.
+admin.site.register(Dish)
+admin.site.register(DishReview)
+admin.site.register(DishQA)
