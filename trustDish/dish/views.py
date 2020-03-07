0f886163cf5144aaee29e62e9969d00a5043e8fb
covee <uniqtop91@gmail.com>
2020-03-07 18:45:05 +0900

    dish list에 대한 url 설정 및 views.py 에 함수 생성

--- trustDish/dish/views.py
+++ trustDish/dish/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Dish
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['GET'])
+def dish_list(request):
+    dishes = Dish.objects.all()
+    
+    #serializer
