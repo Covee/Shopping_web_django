fb043e22418b2e75304ff7c80f7bff6875770271
covee <uniqtop91@gmail.com>
2020-03-09 17:49:19 +0900

    views.py에 serializer 붙여서 response return 해줌

--- trustDish/dish/views.py
+++ trustDish/dish/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .models import Dish
 from rest_framework.decorators import api_view
+from .serializers import DishSerializer
+from rest_framework.response import Response
 
 
 @api_view(['GET'])
 def dish_list(request):
     dishes = Dish.objects.all()
     
-    #serializer
+    serializer = DishSerializer(dishes, many=True)
+    return Response(serializer.data)
+    
