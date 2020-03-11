8e4eba060b7fee373b397ef157a394ca1f65ffce
covee <uniqtop91@gmail.com>
2020-03-11 23:35:49 +0900

    API 자동문서화(swagger) 설치

--- trustDish/dish/urls.py
+++ trustDish/dish/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework_swagger.views import get_swagger_view
+
 
 urlpatterns = [
+    path('docs/', get_swagger_view(title='API Docs')),
     path('dish/', views.dish_list),
     path('dish/<int:dish_id>/', views.dish_detail),
 ]
